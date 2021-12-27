f2265e2915a625e25a99ca16a17d8844d8ceb8d6
queirvz <54514693+queirvz@users.noreply.github.com>
2021-12-27 11:35:32 -0300

    improve the method of stowing files (add -v4 tag and positional arguments)

--- stow_dotfiles.sh
+++ stow_dotfiles.sh
@@ -1,1 +1,3 @@
-stow --target=${HOME} gitconfig p10k selected_editor ssh terminator zsh
+stow --target=${HOME} -v4 ${1}
+
+# the -v4 argument is used to indicate what runs behind the command, allowing one to read verbose about given operation passed to stow

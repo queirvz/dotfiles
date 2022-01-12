328242f3a4516ee8050423a118488f75c8efd453
queirvz <54514693+queirvz@users.noreply.github.com>
2022-01-12 14:58:05 -0300

    modify commented description in stow_dotfiles.sh

--- stow_dotfiles.sh
+++ stow_dotfiles.sh
@@ -1,3 +1,3 @@
 stow --target=${HOME} -v4 ${1}
 
-# the -v4 argument is used to indicate what runs behind the command, allowing one to read verbose about given operation passed to stow
+# the -v4 argument is used to indicate what runs behind the command, allowing one to read detailed verbose about a given operation passed to stow

42619441fb97782e02dbefb209f47d5141c5e0d6
queirvz <54514693+queirvz@users.noreply.github.com>
2022-05-27 17:54:17 -0300

    transform .md note into .sh script

---
+++ windows_terminal_preview/settings_location.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# windows terminal preview settings file location
+
+# C:\Users\GQ\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json
+
+# sidenote: using `stow windows_terminal_preview` might not be productive, given that stashing files via WSL to `/mnt/c/` is not recommended
+
+cd /mnt/c/Users/GQ/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState

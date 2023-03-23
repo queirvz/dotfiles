62484d5e6049d5e1b6f9e9a2e56cacb73da03c8b
Guilherme Queiroz <54514693+queirvz@users.noreply.github.com>
2023-03-23 10:45:24 -0300

    add shebang to script, improve comments

--- stow_dotfiles.sh
+++ stow_dotfiles.sh
@@ -1,3 +1,7 @@
+#! /bin/zsh
+
+# the `-v4` argument is used to indicate what runs behind the command, allowing one to read detailed verbose about a given operation passed to stow
+# see `stow --help`
+
 stow --target=${HOME} -v4 ${1}
 
-# the -v4 argument is used to indicate what runs behind the command, allowing one to read detailed verbose about a given operation passed to stow

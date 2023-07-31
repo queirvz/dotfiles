80bc17c163840b550d09656454f458cd135b18a6
Guilherme Queiroz <54514693+queirvz@users.noreply.github.com>
2023-07-31 09:57:06 -0300

    Add jupyter nbconvert show config script

---
+++ jupyter/nbconvert/show-config.sh
@@ -0,0 +1,11 @@
+#! /bin/zsh
+
+source ~/lab/drumwave/venv/bin activate \
+jupyter nbconvert --show-config-json \
+
+# Run the command and save the output to the file
+jupyter nbconvert --show-config-json > jupyter.nbconvert.show-config.json \
+
+# sudo chmod +x jupyter_nbconvert_show_config.sh \
+
+echo "config saved saved to jupyter.nbconvert.show-config.json"

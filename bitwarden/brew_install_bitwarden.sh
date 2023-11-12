d31f11651874cedfcbfc52c32eb69823ac6c77f6
GQ <gq@machine.local>
2023-11-12 14:09:17 -0300

    Add script to install Bitwarden via Homebrew

---
+++ bitwarden/brew_install_bitwarden.sh
@@ -0,0 +1,13 @@
+#! /bin/zsh
+
+brew install bitwarden bitwarden-cli
+
+```
+==> Downloading https://github.com/bitwarden/clients/releases/download/desktop-v2023.9.1/Bitwarden-2023.9.1-universal.dmg
+==> Downloading from https://objects.githubusercontent.com/github-production-release-asset-2e65be/53538899/b731982d-52df-42ff-8334-758c00ff
+#################################################################################################################################### 100.0%
+==> Installing Cask bitwarden
+==> Moving App 'Bitwarden.app' to '/Applications/Bitwarden.app'
+🍺  bitwarden was successfully installed!
+```
+
